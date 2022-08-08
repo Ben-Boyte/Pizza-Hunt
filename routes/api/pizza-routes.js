046129122774dc6ffeddf68b966bc0246c663c87
Ben-Boyte <zissette1@gmail.com>
2022-08-08 13:28:41 -0500

    added routes into index;

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+  } = require('../../controllers/pizza-controller');
 
 router
   .route('/')
-  .get()
-  .post();
-  
-router
+  .get(getAllPizza)
+  .post(createPizza);
+
+  router
   .route('/:id')
-  .get()
-  .put()
-  .delete();
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
 
 module.exports = router;
