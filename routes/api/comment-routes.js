66db437f367694ac601aebdb230ebd02f74db166
Ben-Boyte <zissette1@gmail.com>
2022-08-10 15:25:28 -0500

    updated pizza controllers

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,15 +1,13 @@
 const router = require('express').Router();
 const { addComment, removeComment } = require('../../controllers/comment-controller');
 
+router.route('/:pizzaId').post(addComment);
+router.route('/:pizzaId/:commentId').delete(removeComment);
+
 const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
 router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
-
-router.route('/:pizzaId').post(addComment);
-
-// /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
 
 module.exports = router;
