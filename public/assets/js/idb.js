d826cea73de0fda95d4e6b92c0e53643f4a571e6
Ben-Boyte <zissette1@gmail.com>
2022-08-13 20:20:56 -0500

    added offline storage

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -34,3 +34,47 @@ request.onsuccess = function(event) {
     
     pizzaObjectStore.add(record);
   }
+
+  function uploadPizza() {
+    
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+  
+    
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+  
+    
+    const getAll = pizzaObjectStore.getAll();
+    getAll.onsuccess = function() {
+        
+        if (getAll.result.length > 0) {
+          fetch('/api/pizzas', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(response => response.json())
+            .then(serverResponse => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+              }
+              
+              const transaction = db.transaction(['new_pizza'], 'readwrite');
+              
+              const pizzaObjectStore = transaction.objectStore('new_pizza');
+              
+              pizzaObjectStore.clear();
+    
+              alert('All saved pizza has been submitted!');
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      };
+    
+  }
+
+  window.addEventListener('online', uploadPizza);
