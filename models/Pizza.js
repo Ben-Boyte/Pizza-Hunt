2555950d4534a7b048df15d7ffe3399f91a9d162
Ben-Boyte <zissette1@gmail.com>
2022-08-13 20:30:20 -0500

    updated pizza controller

--- models/Pizza.js
+++ models/Pizza.js
@@ -2,10 +2,14 @@ const { Schema, model } = require('mongoose');
 const PizzaSchema = new Schema(
   {
     pizzaName: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -13,6 +17,8 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: 'Large'
     },
     toppings: [],
